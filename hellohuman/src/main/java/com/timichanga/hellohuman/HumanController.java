4bb9ec39bb013d5b223c2ae1fce9deaaa3726c04
timmi <timmi@192.168.0.11>
2021-08-13 17:55:18 -0400

    adding hello human assignment

---
+++ hellohuman/src/main/java/com/timichanga/hellohuman/HumanController.java
@@ -0,0 +1,22 @@
+package com.timichanga.hellohuman;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+//rest controller at the top
+@RestController
+public class HumanController {
+	// request mapping for the route
+	@RequestMapping("/")
+	//then the method that it calls
+	// add the request param to see if they added a param
+	// required = false to allow them to not add a param if they don't want to
+	public String name(@RequestParam(value="name", required = false) String name) {
+		if (name == null){
+			return "Hello Human!";
+		}
+		else {
+			return "Hello " + name+ "!";			
+		}
+	}
+}
