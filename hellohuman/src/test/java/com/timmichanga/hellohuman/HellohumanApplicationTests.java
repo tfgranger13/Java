f8017bf541826d68268aca89b9d69020c76a98e8
timmi <timmi@192.168.0.11>
2021-08-13 11:40:59 -0400

    new pc trying to upload strings

---
+++ hellohuman/src/test/java/com/timmichanga/hellohuman/HellohumanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timmichanga.hellohuman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellohumanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ strings/src/main/java/com/timmichanga/strings/StringsApplication.java
@@ -0,0 +1,33 @@
+package com.timmichanga.strings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//2. Importing dependencies
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+//1. Annotation
+@RestController
+
+public class StringsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringsApplication.class, args);
+	}
+	
+	// 1. Annotation
+	@RequestMapping("/")
+    // 3. Method that maps to the request route above
+    public String greet() { // 3
+            return "Hello client, how are you doing?";
+    }
+	@RequestMapping("/random")
+    // 3. Method that maps to the request route above
+    public String random() { // 3
+            return "Spring Boot is great! So easy to just respond with strings.";
+    }
+
+}
