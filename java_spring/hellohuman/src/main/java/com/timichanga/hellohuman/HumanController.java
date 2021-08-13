81e08b29ee9af7e8d6419e650611318693100c13
Your Nametimmichanga13 <timmichanga13@gmail.com>
2021-08-13 18:15:56 -0400

    comments added to human

--- java_spring/hellohuman/src/main/java/com/timichanga/hellohuman/HumanController.java
+++ java_spring/hellohuman/src/main/java/com/timichanga/hellohuman/HumanController.java
@@ -3,12 +3,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-//rest controller at the top
+// rest controller at the top
+// accepts routes
+// also give responses
 @RestController
 public class HumanController {
 	// request mapping for the route
 	@RequestMapping("/")
-	//then the method that it calls
+	// then the method that it calls
 	// add the request param to see if they added a param
 	// required = false to allow them to not add a param if they don't want to
 	public String name(@RequestParam(value="name", required = false) String name) {
