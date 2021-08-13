94286924a2fe9e37f2f2ac78edafd64de4401545
timmi <timmi@192.168.0.11>
2021-08-13 17:19:20 -0400

    I now hate spring tool suite's lack of folder directory

--- routing/src/main/java/com/timmichanga/routing/RoutingApplication.java
+++ routing/src/main/java/com/timmichanga/routing/RoutingApplication.java
@@ -3,11 +3,6 @@ package com.timmichanga.routing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
-
 @SpringBootApplication
 public class RoutingApplication {
 
